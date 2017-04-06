522b30b4fba5df019f9cb48c9532c310a8a813f8
Éric Poirier <eric.poirier7@usherbrooke.ca>
2017-04-06 19:51:05 -0400

    Documentation added

--- cXbase/integration/Connect4Game.h
+++ cXbase/integration/Connect4Game.h
@@ -1,3 +1,38 @@
+/***************************************************************************************************
+ * 
+ * Copyright (C) 2016 Connect X team
+ *
+ * This file is part of Connect X.
+ *
+ * Connect X is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Connect X is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Connect X.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ **************************************************************************************************/
+
+/***********************************************************************************************//**
+ * @file    Connect4Game.h
+ * @author  Eric Poirier
+ * @date    April 2017
+ * @version 0.1
+ *
+ * Interface and implementation for a customized Connect 4 Game object. This object is a 
+ * specialization of the cXbase::Game object.
+ *
+ **************************************************************************************************/
+
+#ifndef CONNECT4GAME_H_
+#define CONNECT4GAME_H_
+
 #include <iostream>
 #include <cxbaseAPI.h>
 
@@ -24,3 +59,5 @@ std::ostream& operator<<(std::ostream& p_flux, const Connect4Game& p_game)
 	
 	return p_flux;
 }
+
+#endif /* CONNECT4GAME_H */_
