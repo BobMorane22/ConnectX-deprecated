89c70b2bd22098276e8d3678df9145bd3032e0b8
Éric Poirier <eric.poirier7@usherbrooke.ca>
2017-05-10 17:59:20 -0400

    Script not added in previous commit

--- cxPythonTools/CreateCopy.py
+++ cxPythonTools/CreateCopy.py
@@ -22,7 +22,15 @@
  #
 #**************************************************************************************************#
 
-
+#***************************************************************************************************
+ # @file        CreateCopy.py
+ # @author      Eric Poirier
+ # @date        May 2017
+ # @version     1
+ #
+ # Script to copy an existing file into an existing directory.
+ #
+#**************************************************************************************************#
 import os
 import shutil
 import sys
@@ -31,75 +39,105 @@ from os.path import isdir
 from os.path import isfile
 from os.path import exists
 
+from sys     import exit
+from shutil  import copy2
+
 
 # Error codes:
-ERROR_NB_ARGS               = 1
-ERROR_SYNTAX                = 2
-ERROR_COPY_A_FILE           = 3
-ERROR_COPY_FILES            = 4
-ERROR_COPY_A_TREE           = 5
-ERROR_OTHER                 = 6
+ERROR_COPY               = 1
+ERROR_NOT_ENOUGH_ARGS    = 2
+ERROR_TOO_MANY_ARGUMENTS = 3
+ERROR_COMMAND_SYNTAX     = 4
 
-# Other:
-ALL_GOOD                    = 0
-MAX_NB_ARGS                 = 4
+#Other:
+EXEC_OK = 0
 
 
 def printHelp():
+    """
+    Prints an help page.
+    """
 
     script = os.path.basename(__file__)
     
-    print("SYNOPSIS:")
-    print("\t" + script + " [-h | --help] [[OPTION] SRC... DEST]\n")
+    print("\nSYNOPSIS:")
+    print("\t" + script + " [-h | --help] [SRC DEST]\n")
+    
+    print("\tDEST must be a previously existing directory.\n")
     
     print("OPTION:")
-    print("\t-t, --tree\tCopy whole tree from SRC to DEST. DEST must not previously exist.\n")
+    print("\t-h, --help\tPrint usage.\n")
     
     print("EXAMPLES:")
-    print("\t" + script + " /home/user/someFile /home/user/someOtherFile")
-    print("\t" + script + " /home/user/someFile /home/user/someDirectory")
-    print("\t" + script + " /home/user/someFile /home/user/someOtherFile /home/user/someDirectory")
-    print("\t" + script + " -t /home/user/someTreeDirectory /home/user/someOtherDirectory")
+    print("\t" + script + " /home/user/someFile /home/user/someDirectory/")
     print("\t" + script + " -h")
-    print("\t" + script + " --help")
+    print("\t" + script + " --help\n")
+    print("\tNote that in the first example, \"/home/user/someDirectory/\"")
+    print("\talready existed before the script was called. Also, the paths")
+    print("\t can be relative.")
+    
 
     
-def createCopy(p_source, p_destination, p_isTree):
+def createCopy(p_source, p_destination):
+    """
+    Copies an existing file into an existing directory.
+    
+    Args:
+        p_source         The path to the file to copy.
+        p_destination    The path to the directory into which to copy the file.
+    """
 
-    # Tree:
-    if p_isTree == True and isdir(p_source) and isdir(p_destination) and not exists(p_destination):
-        shutil.copytree(p_source, p_destination)
-        sys.exit(ALL_GOOD)
+    if isfile(p_source) and isdir(p_destination):
+        copy2(p_source, p_destination)
+        exit(EXEC_OK)
 
-    # One file copy:
-    elif len(p_source) == 1 and isfile(p_source):
-        shutil.copy2(p_source, p_destination)
-        sys.exit(ALL_GOOD)
-
-    # Multiple files copy to same location:
-    elif len(p_source) > 1 and isdir(p_destination):
-
-        allSourcesAreFiles = True
-
-        for source in p_source:
-            if not isfile(source):
-                allSourcesAreFiles = False
-                sys.exit(ERROR_COPY_A_FILE)
-
-        if allSourcesAreFiles:
-            for source in p_source:
-                shutil.copy2(source, p_destination)
-                sys.exit(ALL_GOOD)
-
-    # All other options have failed...
     else:
-        sys.exit(ERROR_OTHER)
+        if not isfile(p_source):
+            print("Error: " + p_source + " is either not a file or does not exist.")
+            
+        if not isdir(p_destination):
+            print("Error: " + p_destination + " is not a directory.")
+            
+        exit(ERROR_COPY)
 
 
 def main(argv):
-    print("Bob")
-    #TODO: Populate main function.
-    #TODO: Add argument parsing.
+    """
+    Copies an existing file into an existing directory.. The user can also call 
+    for help with the following options: "-h" or "--help".
+    
+    Args:
+        argv    A list containing all the arguments.
+    """
+
+    nbArguments = len(argv)
+    
+    # Called without arguments:
+    if nbArguments == 0:
+        print("Error: not enough arguments.")
+        exit(ERROR_NOT_ENOUGH_ARGS)
+
+    # Help requested:
+    if (nbArguments == 1) and (argv[0] in ("-h", "--help")):
+        printHelp()
+        exit(EXEC_OK)
+
+    # File copy:
+    if nbArguments == 2:
+        createCopy(argv[0], argv[1])
+        
+    # Too many arguments:
+    if nbArguments >= 3:
+        print("Error: too many arguments.")
+        exit(ERROR_TOO_MANY_ARGUMENTS)
+
+    try:
+        options, arguments = getopt.gnu_getopt(argv, "t", ["tree="])
+
+    except getopt.GetoptError:
+        script = os.path.basename(__file__)
+        print("Error: command syntax error. Type " + script + " -h or " + script + "--help to see usage.")
+        exit(ERROR_COMMAND_SYNTAX)
 
 
 if __name__ == "__main__":
