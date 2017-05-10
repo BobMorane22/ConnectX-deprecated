7a22b11788796b759ce831a01e1d5c91c13d54a1
Éric Poirier <eric.poirier7@usherbrooke.ca>
2017-05-10 17:49:06 -0400

    Python script to copy a file completed.

---
+++ cxInitTools/init.sh
@@ -0,0 +1,21 @@
+#--------------------------------------------------------------------------------------------------#
+#
+# @file    init.sh
+# @author  Eric Poirier
+# @date    April 2017
+# @version 1
+# @note    Use this file with GNU/Linux systems.
+#
+# This bash script allows the developer to personalize his or her building strategy. You can edit 
+# all commented values to your needs. Predefined values can also be configured, but usually they 
+# are already configured for your system.
+#
+#--------------------------------------------------------------------------------------------------#
+
+#!/bin/bash
+
+export OS            = linux
+export DEBUG         = 1
+
+export CX_SRC_ROOT   = # full path to the main Connect X source directory.
+export CX_BUILD_ROOT = # full path to the main directory into which you want to build Connect X.
---
+++ init.mk
@@ -0,0 +1,55 @@
+#--------------------------------------------------------------------------------------------------#
+#
+# @file    init.mk
+# @author  Eric Poirier
+# @date    April 2017
+# @version 1
+#
+# This file initializes global variables used across most other Makefiles in the code base.
+#
+#--------------------------------------------------------------------------------------------------#
+
+
+# OS:
+# --------------------------------------------------------------------------------------------------
+export OS = linux
+
+# File location:
+# --------------------------------------------------------------------------------------------------
+export ROOT_DIR = $(CURDIR)
+
+# Compilation:
+# --------------------------------------------------------------------------------------------------
+export CPPC                 = g++
+export STANDARD_FLAGS       = -std=c++0x
+
+ifeq ($(DEBUG), 1)
+	export DEBUG_FLAGS          = -g3
+	export OPT_FLAGS            =
+else
+	export DEBUG_FLAGS          =
+	export OPT_FLAGS            = -O0
+endif
+
+export NO_LINKER_FLAGS      = -c
+export WARN_AS_ERRORS_FLAGS = -pedantic-errors\
+                              -Wall\
+                              -Wextra\
+                              -Werror\
+                              -Wconversion
+  
+# Shell:
+# --------------------------------------------------------------------------------------------------
+ifeq ($(OS),linux)
+	export SHELL := /bin/bash
+else ifeq ($(OS),windows)
+	# Reads the wnvironment variable COMSPEC (usually set to CMD.EXE)
+	# See this: https://www.gnu.org/software/make/manual/make.html#Choosing-the-Shell
+	# for more information.
+else
+	export SHELL :=
+endif
+       
+# Cleaning:
+# --------------------------------------------------------------------------------------------------
+export RM = rm -fd
