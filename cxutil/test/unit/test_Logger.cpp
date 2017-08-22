17eb4be46198c9ee2a05495145822d085db58220
Éric Poirier <eric.poirier7@usherbrooke.ca>
2017-08-22 15:57:27 -0400

    TG-102 TG-109 Content moved to test fixture in unit test.

--- cxutil/test/unit/test_Logger.cpp
+++ cxutil/test/unit/test_Logger.cpp
@@ -39,7 +39,43 @@
 
 using namespace cxutil;
 
-TEST(Logger, ConstructorWithParameter)
+class LoggerTests : public::testing::Test
+{
+public:
+    LoggerTests()
+    {
+        std::string separator     {"\\t"                          };
+        std::string endOfLine     {"\\n"                          };
+        std::string lineNumber    {"(\\d)+"                       };
+        std::string executionTime {"(\\d)+\\.(\\d)+"              };    /* "x.y" (where x and y = d...)         */
+        std::string date          {"20\\d\\d/\\d(\\d)?/\\d(\\d)?" };    /* "20ab/x/y" (where x and y = d or dd) */
+        std::string time          {"\\d(\\d)?:\\d(\\d)?:\\d(\\d)?"};    /* "x:y:z (where x, y and z = d or dd)  */
+        std::string info          {"INFO\\s\\s\\s\\s:"            };    /* "INFO    :"                          */
+        std::string warning       {"WARNING\\s:"                  };    /* "WARNING :"                          */
+        std::string error         {"ERROR\\s\\s\\s:"              };    /* "ERROR   :"                          */
+        std::string debug         {"DEBUG\\s\\s\\s:"              };    /* "DEBUG   :"                          */
+        std::string message       {"(.)+"                         };
+
+        std::string header        {"(" + info + "|" + warning + "|" + error + "|" + debug + ")"};
+
+        std::string oneLine       {lineNumber    + separator + 
+                                   executionTime + separator + 
+                                   date          + separator + 
+                                   time          + separator + 
+                                   header        + separator +
+                                   message       + endOfLine   };
+        
+        t_logFileFormat = "(" + oneLine + oneLine + oneLine + oneLine + ")+";
+    };
+
+    std::regex logFileFormat() const {return t_logFileFormat;}
+    void silenceCout() const {std::cout.setstate(std::ios_base::failbit);};
+
+private:
+    std::regex t_logFileFormat;
+};
+
+TEST_F(LoggerTests, ConstructorWithParameter)
 {
     std::ostringstream t_logStream;
     std::string messageInfo    {"Beginning of unit test..."};
@@ -49,42 +85,19 @@ TEST(Logger, ConstructorWithParameter)
     
     Logger t_logger(&t_logStream);
     
-    std::cout.setstate(std::ios_base::failbit); // disable std::cout
+    silenceCout();
     t_logger.logInfo(messageInfo);
     
-    std::cout.setstate(std::ios_base::failbit);
+    silenceCout();
     t_logger.logWarning(messageWarning);
 
-    std::cout.setstate(std::ios_base::failbit);
+    silenceCout();
     t_logger.logError(messageError);
 
-    std::cout.setstate(std::ios_base::failbit);
+    silenceCout();
     t_logger.logDebug(messageDebug);
     
     std::string t_logContents{t_logStream.str()};
     
-    std::string separator     {"\\t"                          };
-    std::string endOfLine     {"\\n"                          };
-    std::string lineNumber    {"(\\d)+"                       };
-    std::string executionTime {"(\\d)+\\.(\\d)+"              };    /* "x.y" (where x and y = d...)         */
-    std::string date          {"20\\d\\d/\\d(\\d)?/\\d(\\d)?" };    /* "20ab/x/y" (where x and y = d or dd) */
-    std::string time          {"\\d(\\d)?:\\d(\\d)?:\\d(\\d)?"};    /* "x:y:z (where x, y and z = d or dd)  */
-    std::string info          {"INFO\\s\\s\\s\\s:"            };    /* "INFO    :"                          */
-    std::string warning       {"WARNING\\s:"                  };    /* "WARNING :"                          */
-    std::string error         {"ERROR\\s\\s\\s:"              };    /* "ERROR   :"                          */
-    std::string debug         {"DEBUG\\s\\s\\s:"              };    /* "DEBUG   :"                          */
-    std::string message       {"(.)+"                         };
-
-    std::string header        {"(" + info + "|" + warning + "|" + error + "|" + debug + ")"};
-
-    std::string oneLine       {lineNumber    + separator + 
-                               executionTime + separator + 
-                               date          + separator + 
-                               time          + separator + 
-                               header        + separator +
-                               message       + endOfLine   };
-    
-    std::regex fileFormat{"(" + oneLine + oneLine + oneLine + oneLine + ")+"};
-    
-    ASSERT_TRUE(std::regex_match(t_logContents, fileFormat));
+    ASSERT_TRUE(std::regex_match(t_logContents, logFileFormat()));
 }
