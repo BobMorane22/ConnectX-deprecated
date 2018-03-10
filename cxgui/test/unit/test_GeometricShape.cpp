cee5a0c6174b9eb66f76e91d0709dd65656f3e7f
Éric Poirier <eric.poirier7@usherbrooke.ca>
2018-03-10 10:50:14 -0500

    TG-190 TG-198 Unit tests fixture created for GeometricShape.

--- cxgui/test/unit/test_GeometricShape.cpp
+++ cxgui/test/unit/test_GeometricShape.cpp
@@ -38,8 +38,9 @@
  * @brief
  *
  *************************************************************************************************/
-class GeometricShapeTests : public cxgui::GeometricShape
+class GeometricShapeNotClosedTests : public cxgui::GeometricShape
 {
+
 public:
 
     /**********************************************************************************************
@@ -47,33 +48,160 @@ public:
      *
      *
      *********************************************************************************************/
-    GeometricShapeTests( const cxutil::Color& p_fillColor       ,
-                         const cxutil::Color& p_backgroundColor ,
-                         const cxutil::Color& p_borderColor     ,
-                         bool p_hasBorder                       ,
-                         double p_borderThickness               ,
-                         cxgui::BorderStyle p_borderStyle       ,
-                         cxgui::BorderDrawer p_drawBorder
-                        ): GeometricShape(p_fillColor,
-                                          p_backgroundColor,
-                                          p_borderColor,
-                                          p_hasBorder,
-                                          p_borderThickness,
-                                          p_borderStyle,
-                                          p_drawBorder)
+    GeometricShapeNotClosedTests( const cxutil::Color& p_fillColor       ,
+                                  const cxutil::Color& p_backgroundColor ,
+                                  const cxutil::Color& p_borderColor     ,
+                                  bool p_hasBorder                       ,
+                                  double p_borderThickness               ,
+                                  cxgui::BorderStyle p_borderStyle
+                                 ): GeometricShape(p_fillColor,
+                                                   p_backgroundColor,
+                                                   p_borderColor,
+                                                   p_hasBorder,
+                                                   p_borderThickness,
+                                                   p_borderStyle)
     {
         // Nothing to do...
     }
 
+private:
 
     /**********************************************************************************************
      * @brief
      *
      *
      *********************************************************************************************/
-    virtual ~GeometricShapeTests() = default;
+    virtual void drawBorder(const Cairo::RefPtr<Cairo::Context>& p_context) const override
+    {
+        const Gtk::Allocation allocation{get_allocation()};
+
+        const int width{allocation.get_width()};
+        const int height{allocation.get_height()};
+
+        const int xCenter{width / 2};
+        const int yCenter{height / 2};
+
+        p_context->move_to(xCenter - width / 4, yCenter - height / 4);
+        p_context->line_to(xCenter + width / 4, yCenter - height / 4);
+        p_context->line_to(xCenter + width / 4, yCenter + height / 4);
+    }
+
 };
 
+
+class GeometricShapeNotSimpleTests : public cxgui::GeometricShape
+{
+
+public:
+
+    /**********************************************************************************************
+     * @brief
+     *
+     *
+     *********************************************************************************************/
+    GeometricShapeNotSimpleTests( const cxutil::Color& p_fillColor       ,
+                                  const cxutil::Color& p_backgroundColor ,
+                                  const cxutil::Color& p_borderColor     ,
+                                  bool p_hasBorder                       ,
+                                  double p_borderThickness               ,
+                                  cxgui::BorderStyle p_borderStyle
+                                 ): GeometricShape(p_fillColor,
+                                                   p_backgroundColor,
+                                                   p_borderColor,
+                                                   p_hasBorder,
+                                                   p_borderThickness,
+                                                   p_borderStyle)
+    {
+        // Nothing to do...
+    }
+
+private:
+
+    /**********************************************************************************************
+     * @brief
+     *
+     *
+     *********************************************************************************************/
+    virtual void drawBorder(const Cairo::RefPtr<Cairo::Context>& p_context) const override
+    {
+        const Gtk::Allocation allocation{get_allocation()};
+
+        const int width{allocation.get_width()};
+        const int height{allocation.get_height()};
+
+        const int xCenter{width / 2};
+        const int yCenter{height / 2};
+
+        p_context->move_to(xCenter - width / 4, yCenter - height / 4);
+        p_context->line_to(xCenter + width / 4, yCenter + height / 4);
+        p_context->line_to(xCenter + width / 4, yCenter - height / 4);
+        p_context->line_to(xCenter - width / 4, yCenter + height / 4);
+        p_context->close_path();
+    }
+
+};
+
+
+class GeometricShapeSimpleAndClosedSquareTests : public cxgui::GeometricShape
+{
+
+public:
+
+    /**********************************************************************************************
+     * @brief
+     *
+     *
+     *********************************************************************************************/
+    GeometricShapeSimpleAndClosedSquareTests( const cxutil::Color& p_fillColor       ,
+                                              const cxutil::Color& p_backgroundColor ,
+                                              const cxutil::Color& p_borderColor     ,
+                                              bool p_hasBorder                       ,
+                                              double p_borderThickness               ,
+                                              cxgui::BorderStyle p_borderStyle       ,
+                                              bool p_makeSureShapeIsClosed
+                                             ): GeometricShape(p_fillColor,
+                                                               p_backgroundColor,
+                                                               p_borderColor,
+                                                               p_hasBorder,
+                                                               p_borderThickness,
+                                                               p_borderStyle),
+                                                               m_makeSureShapeIsClosed{p_makeSureShapeIsClosed}
+    {
+        // Nothing to do...
+    }
+
+private:
+
+    /**********************************************************************************************
+     * @brief
+     *
+     *
+     *********************************************************************************************/
+    virtual void drawBorder(const Cairo::RefPtr<Cairo::Context>& p_context) const override
+    {
+        const Gtk::Allocation allocation{get_allocation()};
+
+        const int width{allocation.get_width()};
+        const int height{allocation.get_height()};
+
+        const int xCenter{width / 2};
+        const int yCenter{height / 2};
+
+        p_context->move_to(xCenter - width / 4, yCenter - height / 4);
+        p_context->line_to(xCenter + width / 4, yCenter - height / 4);
+        p_context->line_to(xCenter + width / 4, yCenter + height / 4);
+        p_context->line_to(xCenter - width / 4, yCenter + height / 4);
+
+        if(m_makeSureShapeIsClosed)
+        {
+            p_context->close_path();
+        }
+    }
+
+
+    const bool m_makeSureShapeIsClosed; ///< Makes sure shape is closed by completing the path.
+
+};
 
 TEST(GeometricShapeTests, NothingYet)
 {
