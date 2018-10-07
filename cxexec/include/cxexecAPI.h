ae6d0c627fd05e834713f35c1bada8206cf30b80
Éric Poirier <eric.poirier7@usherbrooke.ca>
2018-10-07 17:01:56 -0400

    TG-242 API files added for cxgui and cxexec.

---
+++ cxexec/include/cxexecAPI.h
@@ -0,0 +1,54 @@
+/***************************************************************************************************
+ *
+ * Copyright (C) 2018 Connect X team
+ *
+ * This file is part of Connect X.
+ *
+ * Connect X is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Connect X is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Connect X.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ **************************************************************************************************/
+
+/***********************************************************************************************//**
+ * @file    cxexecAPI.h
+ * @author  Eric Poirier
+ * @date    October 2018
+ * @version 1.0
+ *
+ * cxexec library API.
+ *
+ **************************************************************************************************/
+
+#ifndef CXEXECAPI_H_184CDF88_46B8_46C2_A2AF_85B29DB2A461
+#define CXEXECAPI_H_184CDF88_46B8_46C2_A2AF_85B29DB2A461
+
+// UI
+#include "About.h"
+#include "Credits.h"
+#include "CXDisc.h"
+#include "GameBoard.h"
+#include "GameWindow.h"
+#include "GBDisc.h"
+#include "Help.h"
+#include "License.h"
+#include "MessageBox.h"
+#include "NDADisc.h"
+#include "NewGame.h"
+#include "NewPlayersList.h"
+#include "NewPlayerRow.h"
+#include "Window.h"
+
+// Utilities
+#include "util.h"
+
+#endif // CXEXECAPI_H_184CDF88_46B8_46C2_A2AF_85B29DB2A461
---
+++ cxgui/include/cxguiAPI.h
@@ -0,0 +1,53 @@
+/***************************************************************************************************
+ *
+ * Copyright (C) 2018 Connect X team
+ *
+ * This file is part of Connect X.
+ *
+ * Connect X is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Connect X is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Connect X.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ **************************************************************************************************/
+
+/***********************************************************************************************//**
+ * @file    cxguiAPI.h
+ * @author  Eric Poirier
+ * @date    October 2018
+ * @version 1.0
+ *
+ * cxgui library API.
+ *
+ **************************************************************************************************/
+
+#ifndef CXGUIAPI_H_C24826D6_D942_48B2_9198_85A1A11E021A
+#define CXGUIAPI_H_C24826D6_D942_48B2_9198_85A1A11E021A
+
+// UI
+#include "About.h"
+#include "Credits.h"
+#include "cxguiAPI.h"
+#include "Disc.h"
+#include "enums.h"
+#include "GeometricShape.h"
+#include "Help.h"
+#include "License.h"
+#include "MessageBox.h"
+#include "RAIICairoPath.h"
+#include "SectionTitle.h"
+#include "Window.h"
+
+// Utilities
+#include "util.h"
+
+
+#endif // CXGUIAPI_H_C24826D6_D942_48B2_9198_85A1A11E021A
